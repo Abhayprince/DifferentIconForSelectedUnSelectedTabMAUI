bf40969ceadc1ed82248d987627943f83dff88e7
Abhay Prince <46496449+Abhayprince@users.noreply.github.com>
2024-09-21 13:27:24 +0530

    Add project files.

---
+++ DynamicTabbarIconsMAUI/AppShell.xaml.cs
@@ -0,0 +1,52 @@
+﻿namespace DynamicTabbarIconsMAUI;
+
+public partial class AppShell : Shell
+{
+    private static Dictionary<string, (ImageSource SelectedIcon, ImageSource UnselectedIcon)> _tabbarIconsMap =
+        new()
+        {
+            [nameof(MainPage)] = ("home_selected.png", "home.png"),
+            [nameof(ProfilePage)] = ("user_selected.png", "user.png")
+    };
+
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+    
+    protected override void OnNavigating(ShellNavigatingEventArgs args)
+    {
+        // FROM the current tab to new tab
+        // We will set Un-selected Icon here
+
+        base.OnNavigating(args);
+        if(bottomTabbar != null)
+        {
+            var tabbItem = bottomTabbar.CurrentItem.CurrentItem;
+            var route = tabbItem.Route;
+            if (_tabbarIconsMap.TryGetValue(route, out var icons))
+            {
+                tabbItem.Icon = icons.UnselectedIcon;
+            }
+        }
+    }
+    
+    protected override void OnNavigated(ShellNavigatedEventArgs args)
+    {
+        // TO this Tab from previous tab
+        // We will set Selected Icon here
+        base.OnNavigated(args);
+        if (bottomTabbar != null)
+        {
+            var tabbItem = bottomTabbar.CurrentItem.CurrentItem;
+            var route = tabbItem.Route;
+            if (_tabbarIconsMap.TryGetValue(route, out var icons))
+            {
+                tabbItem.Icon = icons.SelectedIcon;
+            }
+        }
+    }
+
+
+}
+
---
+++ DynamicTabbarIconsMAUI/ProfilePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace DynamicTabbarIconsMAUI;
+
+public partial class ProfilePage : ContentPage
+{
+	public ProfilePage()
+	{
+		InitializeComponent();
+	}
+}
